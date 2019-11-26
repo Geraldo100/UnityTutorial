39b0a792e582e1ed242d85b1104676738b046b67
iSeeSounds <58148534+iSeeSounds@users.noreply.github.com>
2019-11-26 23:32:35 +0200

    Finish 1st

--- Bogdan/Assets/Scripts/PlayerScript.cs
+++ Bogdan/Assets/Scripts/PlayerScript.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     public float acceleration; 
     public float speed;
     private Rigidbody rb;
-
+    private int count;
+    public Text countText;
+    public Text WinText;
+    public float HorizontalAccel;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        WinText.text = " ";
     }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical * acceleration);
+        Vector3 movement = new Vector3(moveHorizontal * HorizontalAccel, 0.0f, moveVertical * acceleration);
 
 
         rb.AddForce(movement * speed);
@@ -27,11 +34,18 @@ public class PlayerScript : MonoBehaviour
             if(other.gameObject.CompareTag("Pickup"))
             {
                 other.gameObject.SetActive(false);
+                count = count + 1;
+            SetCountText();
             }
         }
-        
 
-    
+
+    void SetCountText()
+    {
+        countText.text = "count: " + count.ToString();
+        if (count >= 12)
+            WinText.text = "You win!";
+    }
 }
 
 
